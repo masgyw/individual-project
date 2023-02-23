fc219934a9ea81857f0e83bf784076af9df4a7f8
masgyw <masgyw@163.com>
2023-02-23 19:50:18 +0800

    feat:新增领域House

---
+++ backend/app/src/main/java/cn/gyw/individual/backend/app/AppRunner.java
@@ -0,0 +1,26 @@
+package cn.gyw.individual.backend.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 启动后处理
+ *
+ * @date 2023/2/23
+ */
+@Slf4j
+@Component
+public class AppRunner implements ApplicationRunner {
+
+    @Value("${server.port:8080}")
+    private Integer port;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("home page = http://localhost:{}", port);
+        log.info("swagger ui = http://localhost:{}/swagger-ui.html", port);
+    }
+}
---
+++ backend/app/src/main/java/cn/gyw/individual/backend/app/Application.java
@@ -0,0 +1,23 @@
+package cn.gyw.individual.backend.app;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.SpringVersion;
+
+/**
+ * 启动类
+ *
+ * @date 2023/2/23
+ */
+@SpringBootApplication(scanBasePackages = {"cn.gyw.individual.backend"})
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                // 加载spring版本
+                .main(SpringVersion.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+}
