b2ca9ba0f1f474be94b747c81a9014404a21c577
masgyw <masgyw@163.com>
2023-03-16 11:00:53 +0800

    fix:更新open api security

---
+++ backend/app/src/main/java/cn/gyw/individual/backend/app/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package cn.gyw.individual.backend.app.infrastructure.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * swagger open api config
+ *
+ * @date 2023/3/16
+ */
+@Configuration
+public class OpenApiConfig {
+
+    /**
+     * 允许使用或不使用授权头来测试端点
+     */
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("OpenAPI").version("1.0.0"))
+                // Components section defines Security Scheme "secretHeader"
+                .components(new Components()
+                        .addSecuritySchemes("secretHeader", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("token")))
+                // AddSecurityItem section applies created scheme globally
+                .addSecurityItem(new SecurityRequirement().addList("secretHeader"));
+    }
+}
--- starters/security-spring-boot-starter/src/main/java/cn/gyw/individual/starters/security/base/extension/DummyUserContextAware.java
+++ starters/security-spring-boot-starter/src/main/java/cn/gyw/individual/starters/security/base/extension/DummyUserContextAware.java
@@ -5,10 +5,10 @@ import cn.gyw.individual.starters.security.base.BaseJwtUser;
 /**
  * 没有任何信息的用户上下文
  */
-public class DummyUserContextAware implements UserContextAware{
+public class DummyUserContextAware implements UserContextAware {
 
-  @Override
-  public BaseJwtUser processToken(String token) {
-    return new DummyJwtUser();
-  }
+    @Override
+    public BaseJwtUser processToken(String token) {
+        return new DummyJwtUser();
+    }
 }
