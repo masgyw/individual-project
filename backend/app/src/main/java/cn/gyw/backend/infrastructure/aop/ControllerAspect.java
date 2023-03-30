c46c6b7faa278add6c5360f2d343a84f9bee8d82
masgyw <masgyw@163.com>
2023-03-30 19:58:34 +0800

    fix:优化接口日志

--- backend/app/src/main/java/cn/gyw/backend/infrastructure/aop/ControllerAspect.java
+++ backend/app/src/main/java/cn/gyw/backend/infrastructure/aop/ControllerAspect.java
@@ -1,6 +1,10 @@
 package cn.gyw.backend.infrastructure.aop;
 
+import cn.gyw.individual.commons.exceptions.BusinessException;
+import cn.gyw.individual.commons.log.ApiLog;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -17,7 +21,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Controller 切面
@@ -38,15 +45,37 @@ public class ControllerAspect {
     public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-
-        //获取代理地址、请求地址、请求类名、方法名
-        String requestURI = request.getRequestURI();
-        String methodName = joinPoint.getSignature().getName();
         String clazzName = joinPoint.getTarget().getClass().getSimpleName();
         MDC.put("apiCode", clazzName);
-        log.info("requestUri={},methodName={}", requestURI, methodName);
+
+        ApiLog apiLog = new ApiLog();
+        String requestURI = request.getRequestURI();
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        String methodName = method.getName();
+        LocalVariableTableParameterNameDiscoverer parameterNameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+        String[] paramNames = parameterNameDiscoverer.getParameterNames(method);
+        Object[] args = joinPoint.getArgs();
+        List<Object> argList = Arrays.stream(args)
+                .filter(arg -> !(arg instanceof HttpServletRequest))
+                .filter(arg -> !(arg instanceof HttpServletResponse))
+                .collect(Collectors.toList());
+        if (Objects.nonNull(paramNames) && CollectionUtils.isNotEmpty(argList)) {
+            Map<String, Object> reqParam = IntStream.range(0, argList.size()).boxed()
+                    .collect(Collectors.toMap(idx -> paramNames[idx], argList::get));
+            apiLog.setReqData(JSON.toJSONString(reqParam));
+        }
+        apiLog.setMethod(methodName);
+        apiLog.setActionUrl(requestURI);
+        log.info("请求信息：{}", JSON.toJSONString(apiLog));
+
+        long startTime = System.currentTimeMillis();
         Object result;
-        result = joinPoint.proceed();
+        try {
+            result = joinPoint.proceed(args);
+        } catch (Throwable e) {
+            throw e;
+        }
+        log.info("响应信息：{}，执行耗时:{} ms", JSON.toJSONString(result), (System.currentTimeMillis() - startTime));
         return result;
     }
 }
